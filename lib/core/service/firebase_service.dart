6fe0f1de96a572157466d7de3878338322a59bb5
Muahmmed Celal Tok <celaltok2@gmail.com>
2020-02-24 00:58:58 +0300

    student class and futureBuilder added, student listing successful

--- lib/core/service/firebase_service.dart
+++ lib/core/service/firebase_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter_firebase_restfull/core/model/student.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter_firebase_restfull/core/model/user.dart';
 
@@ -16,6 +17,27 @@ class FirebaseSerive {
         final userList =
             jsonModel.map((e) => User.fromJson(e)).toList().cast<User>();
         return userList;
+      default:
+        return Future.error(response.statusCode);
+    }
+  }
+
+  Future<List<Student>> getStudents() async {
+    final response = await http.get("$FIREBASE_URL/students.json");
+
+    switch (response.statusCode) {
+      case HttpStatus.ok:
+        final jsonModel = json.decode(response.body) as Map;
+        final studentList = List<Student>();
+        jsonModel.forEach((key,value){
+          Student student = Student.fromJson(value);
+          student.key = key;
+          studentList.add(student); 
+        });
+
+        
+
+        return studentList;
       default:
         return Future.error(response.statusCode);
     }
--- lib/ui/view/fire_home_view.dart
+++ lib/ui/view/fire_home_view.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_firebase_restfull/core/model/student.dart';
 import 'package:flutter_firebase_restfull/core/model/user.dart';
 import 'package:flutter_firebase_restfull/core/service/firebase_service.dart';
 
@@ -20,9 +21,29 @@ class _FireHomeViewState extends State<FireHomeView> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: FutureBuilder(
-        future: firebaseSerive.getUsers(),
-        builder: (context, snapshot) {
+      body: studentFutureBuilder,
+    );
+  }
+
+  Widget get studentFutureBuilder => FutureBuilder<List<Student>>(
+      future: firebaseSerive.getStudents(),
+      builder: (context, snapshot) {
+        switch (snapshot.connectionState) {
+          case ConnectionState.done:
+            if (snapshot.hasData) {
+              return _listStudent(snapshot.data);
+            } else {
+              return _notFoundWidget;
+            }
+            break;
+          default:
+            return _waitingWidget;
+        }
+      });
+
+  Widget get userFutureBuilder => FutureBuilder<List<User>>(
+      future: firebaseSerive.getUsers(),
+      builder: (context, snapshot) {
         switch (snapshot.connectionState) {
           case ConnectionState.done:
             if (snapshot.hasData) {
@@ -34,20 +55,39 @@ class _FireHomeViewState extends State<FireHomeView> {
           default:
             return _waitingWidget;
         }
-      }),
-    );
-  }
+      });
 
   Widget _listUser(List<User> list) {
-    return Center(child: ListView.builder(itemCount: list.length,itemBuilder:(context,index){
-      return _userCard(list[index]);
-    } ));
+    return Center(
+        child: ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (context, index) {
+              return _userCard(list[index]);
+            }));
   }
 
-  Widget _userCard(User user){
+   Widget _listStudent(List<Student> list) {
+    return Center(
+        child: ListView.builder(
+            itemCount: list.length,
+            itemBuilder: (context, index) {
+              return _studentCard(list[index]);
+            }));
+  }
+
+  Widget _userCard(User user) {
     return Card(
       child: ListTile(
         title: Text(user.name),
+      ),
+    );
+  }
+
+  Widget _studentCard(Student student) {
+    return Card(
+      child: ListTile(
+        title: Text(student.name),
+        subtitle: Text(student.name),
       ),
     );
   }
